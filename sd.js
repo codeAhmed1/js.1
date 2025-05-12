45bfde4fc9b27ede840c8cc1b3fff0d7c4d7b535
codeAhmed1 <ahmed057887@outlook.sa>
2025-05-13 00:01:56 +0300

    Add files via upload

--- sd.js
+++ sd.js
@@ -3,6 +3,9 @@ var box = document.querySelector('.box');
 var yes = document.querySelector('.y');
 var no = document.querySelector('.n');
 bb1.innerHTML = 'اشتراك';
+bb1.classList.add('m1');
+
+
 
 bb1.addEventListener('click',()=>{
 if (bb1.innerHTML == 'إلغاء الاشتراك'){
@@ -10,6 +13,9 @@ if (bb1.innerHTML == 'إلغاء الاشتراك'){
 }else{
 if (bb1.innerHTML == 'اشتراك'){
 bb1.innerHTML = 'تم الاشتراك';
+bb1.classList.remove('m2')
+bb1.classList.remove('m3')
+bb1.classList.add('m1')
 bb1.style.background= 'transparent';
 
 bb1.style.color = 'rgb(255, 255, 255)';
@@ -20,6 +26,9 @@ bb1.style.color = 'rgb(255, 255, 255)';
 yes.addEventListener('click',()=>{
     box.classList.remove('so');
     bb1.innerHTML = 'اشتراك';
+    bb1.classList.remove('m2')
+    bb1.classList.remove('m3')
+    bb1.classList.add('m1')
     bb1.style.background= '#fff';
     bb1.style.color = 'rgb(0, 0, 0)';
     
@@ -27,33 +36,47 @@ yes.addEventListener('click',()=>{
 
 no.addEventListener('click',()=>{
     box.classList.remove('so');
+    bb1.classList.remove('m2')
+    bb1.classList.remove('m3')
+    bb1.classList.add('m1')
 
 })
-
+//عند وضع الموس
 bb1.addEventListener("mousemove",()=>{
 if (bb1.innerHTML == 'تم الاشتراك'){
     bb1.innerHTML = 'إلغاء الاشتراك';
+    bb1.classList.remove('m1')
+    bb1.classList.remove('m2')
+    bb1.classList.add('m3')
     bb1.style.background= '#f11111';
     bb1.style.css = 'rgb(0, 0, 0)';
     bb1.style.color = 'rgb(255, 255, 255)';
 }else{
     if (bb1.innerHTML== 'اشتراك'){
-  
+    bb1.classList.remove('m1')
+    bb1.classList.remove('m3')
+    bb1.classList.add('m2')
     bb1.style.background= 'rgb(4, 138, 255)';
     bb1.style.css = 'rgb(0, 0, 0)';
     bb1.style.color = 'rgb(255, 255, 255)';
+    
 }}
 });
-
+//عند اخراج الموس
 bb1.addEventListener("mouseout",()=>{
     if (bb1.innerHTML == 'إلغاء الاشتراك'){  
     bb1.innerHTML = 'تم الاشتراك';
+    bb1.classList.remove('m2')
+    bb1.classList.remove('m3')
+    bb1.classList.add('m1')
     bb1.style.background= 'transparent';
 
     bb1.style.color = 'rgb(255, 255, 255)';
 }else{
 if (bb1.innerHTML== 'اشتراك'){
-
+    bb1.classList.remove('m2')
+    bb1.classList.remove('m3')
+    bb1.classList.add('m1')
     bb1.style.background= '#fff';
     bb1.style.color = 'rgb(0, 0, 0)';
 }
